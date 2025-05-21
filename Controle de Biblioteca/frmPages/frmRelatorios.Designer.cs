60a01b2c23b9e5d73fe51ed3e8b798de7536df83
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-05-21 05:45:50 -0300

    feat: add no data validation in relatorios tables

--- Controle de Biblioteca/frmPages/frmRelatorios.Designer.cs
+++ Controle de Biblioteca/frmPages/frmRelatorios.Designer.cs
@@ -41,11 +41,20 @@
             this.dtGridEmprestimos = new System.Windows.Forms.DataGridView();
             this.label5 = new System.Windows.Forms.Label();
             this.dtGridLivrosDisponiveis = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dtGridLivrosEmprestados = new System.Windows.Forms.DataGridView();
+            this.noDataAlunos = new System.Windows.Forms.Label();
+            this.noDataProfessores = new System.Windows.Forms.Label();
+            this.noDataLivros = new System.Windows.Forms.Label();
+            this.noDataLivrosDisponiveis = new System.Windows.Forms.Label();
+            this.noDataLivrosEmprestados = new System.Windows.Forms.Label();
+            this.noDataHistoricoDeEmprestimos = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dtGridAlunos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dtGridLivros)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dtGridProfessores)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dtGridEmprestimos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dtGridLivrosDisponiveis)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridLivrosEmprestados)).BeginInit();
             this.SuspendLayout();
             // 
             // dtGridAlunos
@@ -57,6 +66,7 @@
             this.dtGridAlunos.RowTemplate.Height = 24;
             this.dtGridAlunos.Size = new System.Drawing.Size(950, 145);
             this.dtGridAlunos.TabIndex = 0;
+            this.dtGridAlunos.Visible = false;
             this.dtGridAlunos.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dtGridAlunos_DataBindingComplete);
             // 
             // dtGridLivros
@@ -68,6 +78,7 @@
             this.dtGridLivros.RowTemplate.Height = 24;
             this.dtGridLivros.Size = new System.Drawing.Size(950, 145);
             this.dtGridLivros.TabIndex = 1;
+            this.dtGridLivros.Visible = false;
             // 
             // label8
             // 
@@ -133,6 +144,7 @@
             this.dtGridProfessores.RowTemplate.Height = 24;
             this.dtGridProfessores.Size = new System.Drawing.Size(950, 145);
             this.dtGridProfessores.TabIndex = 64;
+            this.dtGridProfessores.Visible = false;
             this.dtGridProfessores.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dtGridProfessores_DataBindingComplete);
             // 
             // btnAtualizar
@@ -149,7 +161,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(9, 465);
+            this.label4.Location = new System.Drawing.Point(1017, 472);
             this.label4.MaximumSize = new System.Drawing.Size(800, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(181, 18);
@@ -159,12 +171,13 @@
             // dtGridEmprestimos
             // 
             this.dtGridEmprestimos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtGridEmprestimos.Location = new System.Drawing.Point(12, 486);
+            this.dtGridEmprestimos.Location = new System.Drawing.Point(1020, 493);
             this.dtGridEmprestimos.Name = "dtGridEmprestimos";
             this.dtGridEmprestimos.RowHeadersWidth = 51;
             this.dtGridEmprestimos.RowTemplate.Height = 24;
             this.dtGridEmprestimos.Size = new System.Drawing.Size(950, 145);
             this.dtGridEmprestimos.TabIndex = 67;
+            this.dtGridEmprestimos.Visible = false;
             // 
             // label5
             // 
@@ -186,12 +199,115 @@
             this.dtGridLivrosDisponiveis.RowTemplate.Height = 24;
             this.dtGridLivrosDisponiveis.Size = new System.Drawing.Size(950, 145);
             this.dtGridLivrosDisponiveis.TabIndex = 69;
+            this.dtGridLivrosDisponiveis.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 472);
+            this.label6.MaximumSize = new System.Drawing.Size(800, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(141, 18);
+            this.label6.TabIndex = 72;
+            this.label6.Text = "Livros Emprestados";
+            // 
+            // dtGridLivrosEmprestados
+            // 
+            this.dtGridLivrosEmprestados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGridLivrosEmprestados.Location = new System.Drawing.Point(12, 493);
+            this.dtGridLivrosEmprestados.Name = "dtGridLivrosEmprestados";
+            this.dtGridLivrosEmprestados.RowHeadersWidth = 51;
+            this.dtGridLivrosEmprestados.RowTemplate.Height = 24;
+            this.dtGridLivrosEmprestados.Size = new System.Drawing.Size(950, 145);
+            this.dtGridLivrosEmprestados.TabIndex = 71;
+            this.dtGridLivrosEmprestados.Visible = false;
+            // 
+            // noDataAlunos
+            // 
+            this.noDataAlunos.AutoSize = true;
+            this.noDataAlunos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noDataAlunos.Location = new System.Drawing.Point(12, 121);
+            this.noDataAlunos.MaximumSize = new System.Drawing.Size(800, 0);
+            this.noDataAlunos.Name = "noDataAlunos";
+            this.noDataAlunos.Size = new System.Drawing.Size(404, 25);
+            this.noDataAlunos.TabIndex = 73;
+            this.noDataAlunos.Text = "Não existem dados para a tabela de \"Alunos\"";
+            this.noDataAlunos.Visible = false;
+            // 
+            // noDataProfessores
+            // 
+            this.noDataProfessores.AutoSize = true;
+            this.noDataProfessores.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noDataProfessores.Location = new System.Drawing.Point(1018, 121);
+            this.noDataProfessores.MaximumSize = new System.Drawing.Size(800, 0);
+            this.noDataProfessores.Name = "noDataProfessores";
+            this.noDataProfessores.Size = new System.Drawing.Size(426, 25);
+            this.noDataProfessores.TabIndex = 74;
+            this.noDataProfessores.Text = "Não existem dados para a tabela de \"Professor\"";
+            this.noDataProfessores.Visible = false;
+            // 
+            // noDataLivros
+            // 
+            this.noDataLivros.AutoSize = true;
+            this.noDataLivros.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noDataLivros.Location = new System.Drawing.Point(12, 310);
+            this.noDataLivros.MaximumSize = new System.Drawing.Size(800, 0);
+            this.noDataLivros.Name = "noDataLivros";
+            this.noDataLivros.Size = new System.Drawing.Size(395, 25);
+            this.noDataLivros.TabIndex = 75;
+            this.noDataLivros.Text = "Não existem dados para a tabela de \"Livros\"";
+            this.noDataLivros.Visible = false;
+            // 
+            // noDataLivrosDisponiveis
+            // 
+            this.noDataLivrosDisponiveis.AutoSize = true;
+            this.noDataLivrosDisponiveis.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noDataLivrosDisponiveis.Location = new System.Drawing.Point(1018, 310);
+            this.noDataLivrosDisponiveis.MaximumSize = new System.Drawing.Size(800, 0);
+            this.noDataLivrosDisponiveis.Name = "noDataLivrosDisponiveis";
+            this.noDataLivrosDisponiveis.Size = new System.Drawing.Size(500, 25);
+            this.noDataLivrosDisponiveis.TabIndex = 76;
+            this.noDataLivrosDisponiveis.Text = "Não existem dados para a tabela de \"Livros Disponíveis\"";
+            this.noDataLivrosDisponiveis.Visible = false;
+            // 
+            // noDataLivrosEmprestados
+            // 
+            this.noDataLivrosEmprestados.AutoSize = true;
+            this.noDataLivrosEmprestados.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noDataLivrosEmprestados.Location = new System.Drawing.Point(12, 505);
+            this.noDataLivrosEmprestados.MaximumSize = new System.Drawing.Size(800, 0);
+            this.noDataLivrosEmprestados.Name = "noDataLivrosEmprestados";
+            this.noDataLivrosEmprestados.Size = new System.Drawing.Size(515, 25);
+            this.noDataLivrosEmprestados.TabIndex = 77;
+            this.noDataLivrosEmprestados.Text = "Não existem dados para a tabela de \"Livros Emprestados\"";
+            this.noDataLivrosEmprestados.Visible = false;
+            // 
+            // noDataHistoricoDeEmprestimos
+            // 
+            this.noDataHistoricoDeEmprestimos.AutoSize = true;
+            this.noDataHistoricoDeEmprestimos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noDataHistoricoDeEmprestimos.Location = new System.Drawing.Point(1018, 505);
+            this.noDataHistoricoDeEmprestimos.MaximumSize = new System.Drawing.Size(800, 0);
+            this.noDataHistoricoDeEmprestimos.Name = "noDataHistoricoDeEmprestimos";
+            this.noDataHistoricoDeEmprestimos.Size = new System.Drawing.Size(563, 25);
+            this.noDataHistoricoDeEmprestimos.TabIndex = 78;
+            this.noDataHistoricoDeEmprestimos.Text = "Não existem dados para a tabela de \"Histórico de Empréstimos\"";
+            this.noDataHistoricoDeEmprestimos.Visible = false;
             // 
             // Relatórios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1982, 716);
+            this.Controls.Add(this.noDataHistoricoDeEmprestimos);
+            this.Controls.Add(this.noDataLivrosEmprestados);
+            this.Controls.Add(this.noDataLivrosDisponiveis);
+            this.Controls.Add(this.noDataLivros);
+            this.Controls.Add(this.noDataProfessores);
+            this.Controls.Add(this.noDataAlunos);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dtGridLivrosEmprestados);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.dtGridLivrosDisponiveis);
             this.Controls.Add(this.label4);
@@ -213,6 +329,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dtGridProfessores)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dtGridEmprestimos)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dtGridLivrosDisponiveis)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridLivrosEmprestados)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -233,5 +350,13 @@
         private System.Windows.Forms.DataGridView dtGridEmprestimos;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.DataGridView dtGridLivrosDisponiveis;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dtGridLivrosEmprestados;
+        private System.Windows.Forms.Label noDataAlunos;
+        private System.Windows.Forms.Label noDataProfessores;
+        private System.Windows.Forms.Label noDataLivros;
+        private System.Windows.Forms.Label noDataLivrosDisponiveis;
+        private System.Windows.Forms.Label noDataLivrosEmprestados;
+        private System.Windows.Forms.Label noDataHistoricoDeEmprestimos;
     }
 }
--- Controle de Biblioteca/frmPages/frmRelatorios.cs
+++ Controle de Biblioteca/frmPages/frmRelatorios.cs
@@ -42,63 +42,134 @@ namespace Controle_de_Biblioteca.frmPages
         void CarregarDados()
         {
             //Alunos
-            dtGridAlunos.DataSource = null;
-            dtGridAlunos.DataSource = mainForm.usuarios
-                .Where(u => u.tipo == TipoUsuario.Aluno)
-                .Cast<Aluno>()
-                .ToList();
-            dtGridAlunos.Columns["nome"].HeaderText = "Nome do Aluno";
-            dtGridAlunos.Columns["tipo"].HeaderText = "Tipo";
-            dtGridAlunos.Columns["ra"].HeaderText = "RA do Aluno";
-            dtGridAlunos.Columns["curso"].HeaderText = "Curso do Aluno";
+            if (mainForm.usuarios.Count(u => u.tipo == TipoUsuario.Aluno) >= 1)
+            {
+                dtGridAlunos.DataSource = null;
+                dtGridAlunos.DataSource = mainForm.usuarios
+                    .Where(u => u.tipo == TipoUsuario.Aluno)
+                    .Cast<Aluno>()
+                    .ToList();
+                dtGridAlunos.Columns["nome"].HeaderText = "Nome do Aluno";
+                dtGridAlunos.Columns["tipo"].HeaderText = "Tipo";
+                dtGridAlunos.Columns["ra"].HeaderText = "RA do Aluno";
+                dtGridAlunos.Columns["curso"].HeaderText = "Curso do Aluno";
+
+                noDataAlunos.Visible = false;
+                dtGridAlunos.Visible = true;
+            } else
+            {
+                noDataAlunos.Visible = true;
+                dtGridAlunos.Visible = false;
+            }
 
             //Professores
-            dtGridProfessores.DataSource = null;
-            dtGridProfessores.DataSource = mainForm.usuarios
-                .Where(u => u.tipo == TipoUsuario.Professor)
-                .Cast<Professor>()
-                .ToList();
-            dtGridProfessores.Columns["nome"].HeaderText = "Nome do Professor";
-            dtGridProfessores.Columns["tipo"].HeaderText = "Tipo";
-            dtGridProfessores.Columns["numeroRegistro"].HeaderText = "Número de Registro";
-            dtGridProfessores.Columns["departamento"].HeaderText = "Departamento";
+            if(mainForm.usuarios.Count(u => u.tipo == TipoUsuario.Professor) >= 1)
+            {
+                dtGridProfessores.DataSource = null;
+                dtGridProfessores.DataSource = mainForm.usuarios
+                    .Where(u => u.tipo == TipoUsuario.Professor)
+                    .Cast<Professor>()
+                    .ToList();
+                dtGridProfessores.Columns["nome"].HeaderText = "Nome do Professor";
+                dtGridProfessores.Columns["tipo"].HeaderText = "Tipo";
+                dtGridProfessores.Columns["numeroRegistro"].HeaderText = "Número de Registro";
+                dtGridProfessores.Columns["departamento"].HeaderText = "Departamento";
+
+                noDataProfessores.Visible = false;
+                dtGridProfessores.Visible = true;
+            } else
+            {
+                noDataProfessores.Visible = true;
+                dtGridProfessores.Visible = false;
+            }
 
             //Livros
-            dtGridLivros.DataSource = null;
-            dtGridLivros.DataSource = mainForm.livros.ToList();
-            dtGridLivros.Columns["titulo"].HeaderText = "Título do Livro";
-            dtGridLivros.Columns["autor"].HeaderText = "Autor do Livro";
-            dtGridLivros.Columns["ano"].HeaderText = "Ano de Publicação";
-            dtGridLivros.Columns["isbn"].HeaderText = "ISBN do Livro";
-            dtGridLivros.Columns["status"].HeaderText = "Situação do Livro";
+            if (mainForm.livros.Count >= 1)
+            {
+                dtGridLivros.DataSource = null;
+                dtGridLivros.DataSource = mainForm.livros.ToList();
+                dtGridLivros.Columns["titulo"].HeaderText = "Título do Livro";
+                dtGridLivros.Columns["autor"].HeaderText = "Autor do Livro";
+                dtGridLivros.Columns["ano"].HeaderText = "Ano de Publicação";
+                dtGridLivros.Columns["isbn"].HeaderText = "ISBN do Livro";
+                dtGridLivros.Columns["status"].HeaderText = "Situação do Livro";
+
+                noDataLivros.Visible = false;
+                dtGridLivros.Visible = true;
+            } else
+            {
+                noDataLivros.Visible = true;
+                dtGridLivros.Visible = false;
+            }
 
             //Livros Disponíveis
-            dtGridLivrosDisponiveis.DataSource = null;
-            dtGridLivrosDisponiveis.DataSource = mainForm.livros
-                .Where(l => l.status == StatusLivro.Disponivel)
-                .ToList();
-            dtGridLivrosDisponiveis.Columns["titulo"].HeaderText = "Título do Livro";
-            dtGridLivrosDisponiveis.Columns["autor"].HeaderText = "Autor do Livro";
-            dtGridLivrosDisponiveis.Columns["ano"].HeaderText = "Ano de Publicação";
-            dtGridLivrosDisponiveis.Columns["isbn"].HeaderText = "ISBN do Livro";
-            dtGridLivrosDisponiveis.Columns["status"].HeaderText = "Situação do Livro";
+            if (mainForm.livros.Count(l => l.status == StatusLivro.Disponivel) >= 1)
+            {
+                dtGridLivrosDisponiveis.DataSource = null;
+                dtGridLivrosDisponiveis.DataSource = mainForm.livros
+                    .Where(l => l.status == StatusLivro.Disponivel)
+                    .ToList();
+                dtGridLivrosDisponiveis.Columns["titulo"].HeaderText = "Título do Livro";
+                dtGridLivrosDisponiveis.Columns["autor"].HeaderText = "Autor do Livro";
+                dtGridLivrosDisponiveis.Columns["ano"].HeaderText = "Ano de Publicação";
+                dtGridLivrosDisponiveis.Columns["isbn"].HeaderText = "ISBN do Livro";
+                dtGridLivrosDisponiveis.Columns["status"].HeaderText = "Situação do Livro";
+
+                noDataLivrosDisponiveis.Visible = false;
+                dtGridLivrosDisponiveis.Visible = true;
+            } else
+            {
+                noDataLivrosDisponiveis.Visible = true;
+                dtGridLivrosDisponiveis.Visible = false;
+            }
+
+            //Livros Emprestados
+            if (mainForm.livros.Count(l => l.status == StatusLivro.Emprestado) >= 1)
+            {
+                dtGridLivrosEmprestados.DataSource = null;
+                dtGridLivrosEmprestados.DataSource = mainForm.livros
+                    .Where(l => l.status == StatusLivro.Emprestado)
+                    .ToList();
+                dtGridLivrosEmprestados.Columns["titulo"].HeaderText = "Título do Livro";
+                dtGridLivrosEmprestados.Columns["autor"].HeaderText = "Autor do Livro";
+                dtGridLivrosEmprestados.Columns["ano"].HeaderText = "Ano de Publicação";
+                dtGridLivrosEmprestados.Columns["isbn"].HeaderText = "ISBN do Livro";
+                dtGridLivrosEmprestados.Columns["status"].HeaderText = "Situação do Livro";
+
+                noDataLivrosEmprestados.Visible = false;
+                dtGridLivrosEmprestados.Visible = true;
+            } else
+            {
+                noDataLivrosEmprestados.Visible = true;
+                dtGridLivrosEmprestados.Visible = false;
+            }
 
             //Emprestimos
-            dtGridEmprestimos.DataSource = null;
-            dtGridEmprestimos.DataSource = mainForm.emprestimos.Select(e => new
+            if (mainForm.emprestimos.Count >= 1)
             {
-                ID = e._id,
-                usuario = e.usuario.nome,
-                livro = e.livro.titulo,
-                data = e.data.ToString("d"),
-                dataDevolucao = e.dataDevolucao.ToString("d"),
-                status = e.status.ToString()
-            }).ToList();
-            dtGridEmprestimos.Columns["usuario"].HeaderText = "Nome do Usuário";
-            dtGridEmprestimos.Columns["livro"].HeaderText = "Título do Livro";
-            dtGridEmprestimos.Columns["data"].HeaderText = "Data de Empréstimo";
-            dtGridEmprestimos.Columns["dataDevolucao"].HeaderText = "Data de Devolução";
-            dtGridEmprestimos.Columns["status"].HeaderText = "Situação";
+                dtGridEmprestimos.DataSource = null;
+                dtGridEmprestimos.DataSource = mainForm.emprestimos.Select(e => new
+                {
+                    ID = e._id,
+                    usuario = e.usuario.nome,
+                    livro = e.livro.titulo,
+                    data = e.data.ToString("g"),
+                    dataDevolucao = e.dataDevolucao.ToString("g"),
+                    status = e.status.ToString()
+                }).ToList();
+                dtGridEmprestimos.Columns["usuario"].HeaderText = "Nome do Usuário";
+                dtGridEmprestimos.Columns["livro"].HeaderText = "Título do Livro";
+                dtGridEmprestimos.Columns["data"].HeaderText = "Data de Empréstimo";
+                dtGridEmprestimos.Columns["dataDevolucao"].HeaderText = "Data de Devolução";
+                dtGridEmprestimos.Columns["status"].HeaderText = "Situação";
+
+                noDataHistoricoDeEmprestimos.Visible = false;
+                dtGridEmprestimos.Visible = true;
+            } else
+            {
+                noDataHistoricoDeEmprestimos.Visible = true;
+                dtGridEmprestimos.Visible = false;
+            }
         }
 
         private void frmRelatorios_Load(object sender, EventArgs e)
